1e0a87c4c9641c74cdb6929fd54e907492a7b742
joncastillo <me@joncastillo.org>
2020-12-06 11:12:53 +1100

    Lower current, too much motor noise.

--- Firmware/Configuration_prusa.h
+++ Firmware/Configuration_prusa.h
@@ -265,8 +265,8 @@
 #define TMC2130_SG_THRS_HOME {3, 3, TMC2130_SG_THRS_Z, TMC2130_SG_THRS_E}
 
 //new settings is possible for vsense = 1, running current value > 31 set vsense to zero and shift both currents by 1 bit right (Z axis only)
-#define TMC2130_CURRENTS_H {20, 20, 35, 31}  // default holding currents for all axes
-#define TMC2130_CURRENTS_R {20, 20, 35, 31}  // default running currents for all axes
+#define TMC2130_CURRENTS_H {16, 16, 35, 30}  // default holding currents for all axes
+#define TMC2130_CURRENTS_R {16, 16, 35, 30}  // default running currents for all axes
 #define TMC2130_CURRENTS_R_HOME {8, 10, 20, 18}  // homing running currents for all axes
 // #define TMC2130_UNLOAD_CURRENT_R 12			 // lower current for M600 to protect filament sensor - Unused
 
@@ -507,8 +507,8 @@
 #define PC_PREHEAT_HOTEND_TEMP 275
 #define PC_PREHEAT_HPB_TEMP 110
 
-#define ABS_PREHEAT_HOTEND_TEMP 250
-#define ABS_PREHEAT_HPB_TEMP 120
+#define ABS_PREHEAT_HOTEND_TEMP 245
+#define ABS_PREHEAT_HPB_TEMP 115
 
 #define HIPS_PREHEAT_HOTEND_TEMP 220
 #define HIPS_PREHEAT_HPB_TEMP 100
